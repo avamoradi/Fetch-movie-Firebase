3646f63b85389172f87d630f3be758f4fbd6d3cb
avamoradi <ava.moradi86@gmail.com>
2021-08-02 14:07:36 +0200

    add all files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, { useState, useEffect, useCallback } from 'react'
+import MoviesList from './components/MoviesList'
+import AddMovie from './components/AddMovie'
 
 function App() {
+  const [movies, setMovies] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
+
+  const fetchMoviesHandler = useCallback(async () => {
+    setIsLoading(true)
+    setError(null)
+    try {
+      // const response = await fetch('https://swapi.dev/api/films')
+      const response = await fetch(
+        'https://react-http-f1b29-default-rtdb.europe-west1.firebasedatabase.app/movies.json'
+      )
+
+      if (!response.ok) {
+        throw new Error('Something went wrong')
+      }
+      const data = await response.json()
+
+      const loadMovies = []
+
+      for (const Key in data) {
+        loadMovies.push({
+          id: Key,
+          title: data[Key].title,
+          openingText: data[Key].openingText,
+          releaseDate: data[Key].releaseDate,
+        })
+      }
+
+      // const transformedMovies = data.results.map((movieData) => {
+      //   return {
+      //     id: movieData.episode_id,
+      //     title: movieData.title,
+      //     openingText: movieData.opening_crawl,
+      //     releaseDate: movieData.release_date,
+      //   }
+      // })
+
+      setMovies(loadMovies)
+    } catch (error) {
+      setError(error.message)
+    }
+    setIsLoading(false)
+  }, [])
+
+  useEffect(() => {
+    fetchMoviesHandler()
+  }, [fetchMoviesHandler])
+
+  async function addMovieHandler(movie) {
+    const response = await fetch(
+      'https://react-http-f1b29-default-rtdb.europe-west1.firebasedatabase.app/movies.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(movie),
+        headers: { 'Content-Type': 'applicatipn/json' },
+      }
+    )
+
+    const data = await response.json()
+    console.log(data)
+  }
+
+  let content = <p>Found no movies.</p>
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />
+  }
+  if (error) {
+    content = <p>{error}</p>
+  }
+  if (isLoading) {
+    content = <p>Loading...</p>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>{content}</section>
+    </React.Fragment>
+  )
 }
 
-export default App;
+export default App
